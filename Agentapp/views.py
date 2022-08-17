cbc6f6c7205bd52b005efc65d49251f74a3bc01e
timmi <danieloluwoleoluwatimileyin@gmail.com>
2022-08-17 12:22:24 +0100

    fixed automatically assigning the organisation id

--- Agentapp/views.py
+++ Agentapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import reverse
 from django.views import generic
 from CRMapp.models import Agent
 from .forms import AgentCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class Agent_list(generic.ListView):
     template_name = 'agent-list.html'
@@ -11,16 +12,18 @@ class Agent_list(generic.ListView):
     def get_success_url(self):
         return reverse('leads:lead-list')
 
-class Agent_create(generic.CreateView):
+class Agent_create(LoginRequiredMixin, generic.CreateView):
     template_name= 'agent-create.html'
     form_class = AgentCreationForm
+
+    def get_success_url(self):
+        return reverse('agents:agent-list')
 
     #for automatically generating the organisation
     def form_valid(self, form):
         agent = form.save(commit=False)
         agent.organisation = self.request.user.auto
         agent.save()
-        return super(Agent_create,self).form_valid(form)
+        return super(Agent_create,self).form_valid(form)                                                                       
 
-    def get_success_url(self):
-        return reverse('agents:agent-list')
+
--- CRMapp/models.py
+++ CRMapp/models.py
@@ -24,7 +24,7 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField("User", on_delete=models.CASCADE)
-    Organisation = models.ForeignKey("Auto", on_delete=models.CASCADE)
+    organisation = models.ForeignKey("Auto", on_delete=models.CASCADE)
     def __str__(self):
         return self.user.email
 
