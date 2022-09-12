b068c579edd78847e175e68c33a51ebfab9799f9
Timmi <danieoluwoleoluwatimileyin@gmail.com>
2022-09-12 13:55:07 +0100

    styling

--- CRM/settings.py
+++ CRM/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
